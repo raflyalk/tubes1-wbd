40aaf2225ae1c2c8f38f1a2f40f540ffffc1370a
M. Rafli Al Khadafi <raflyalk@gmail.com>
2018-10-18 09:14:36 +0700

    Restructure folder to MVC

--- controller/login.php
+++ controller/login.php
@@ -1,6 +1,6 @@
 <?php
     if (isset($_POST["username"])) {
-        $redirectLink = "/page/test-login";
+        $redirectLink = "/view/test-login";
         setcookie("username", $_POST["username"], time() + 1800, "/");
         header("Location: " . $redirectLink);
         exit();
