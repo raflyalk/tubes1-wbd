620e87bc02b9f35d7fc0cc79b0b27b1e31b0550d
rrifaldiu <13516098@std.stei.itb.ac.id>
2018-10-23 11:01:57 +0700

    add css file

--- view/register/index.php
+++ view/register/index.php
@@ -3,42 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <title>Register</title>
-    <!-- <link type="text/css" rel="stylesheet" href="/view/register/register.css"> -->
+    <link type="text/css" rel="stylesheet" href="/view/register/register.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Pathway+Gothic+One" rel="stylesheet">
 </head>
 <body>
-    <form action="">
-        <div class="form-input">
-            <label for="fullname">Name</label>
-            <input type="text" id="fullname" name="fullname" size="20">
+    <div class="register-box">
+    <h1>REGISTER</h1>
+    <form class="register-form" action="" method="post">
+        <div class="input-field-container">
+            <div class="form-input">
+                <label for="fullname">Name</label>
+                <input type="text" id="fullname" name="fullname" size="20">
+            </div>
+            <div class="form-input">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" size="20">
+            </div>
+            <div class="form-input">
+                <label for="email">Email</label>
+                <input type="text" id="email" name="email" size="20">
+            </div>
+            <div class="form-input">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" size="20">
+            </div>
+            <div class="form-input">
+                <label for="confirmpassword">Congirm Password</label>
+                <input type="password" id="confirmpassword" name="confirmpassword" size="20">
+            </div>
+            <div class="form-input">
+                <label for="address">Address</label>
+                <input type="text" id="address" name="address" size="20">
+            </div>
+            <div class="form-input">
+                <label for="phonenum">Phone Number</label>
+                <input type="text" id="phonenum" name="phonenum" size="20">
+            </div>
         </div>
-        <div class="form-input">
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username" size="20">
+        <div id="login-button">
+            <a href="/view/login">Already have an account?</a>
         </div>
-        <div class="form-input">
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" size="20">
-        </div>
-        <div class="form-input">
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" size="20">
-        </div>
-        <div class="form-input">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" size="20">
-        </div>
-        <div class="form-input">
-            <label for="confirmpassword">Congirm Password</label>
-            <input type="password" id="confirmpassword" name="confirmpassword" size="20">
-        </div>
-        <div class="form-input">
-            <label for="address">Address</label>
-            <input type="text" id="address" name="address" size="100">
-        </div>
-        <div class="form-input">
-            <label for="phonenum">Phone Number</label>
-            <input type="text" id="phonenum" name="phonenum" size="15">
+        <div class="register-button">
+            <input id="register-button" type="submit" value="REGISTER">
         </div>
     </form>
 </body>
