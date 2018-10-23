cffa799b767760b8bcb90d56c21d34dcb9434e17
rrifaldiu <13516098@std.stei.itb.ac.id>
2018-10-23 10:27:19 +0700

    initialize register form

---
+++ view/register/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+    <!-- <link type="text/css" rel="stylesheet" href="/view/register/register.css"> -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Pathway+Gothic+One" rel="stylesheet">
+</head>
+<body>
+    <form action="">
+        <div class="form-input">
+            <label for="fullname">Name</label>
+            <input type="text" id="fullname" name="fullname" size="20">
+        </div>
+        <div class="form-input">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" size="20">
+        </div>
+        <div class="form-input">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" size="20">
+        </div>
+        <div class="form-input">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" size="20">
+        </div>
+        <div class="form-input">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" size="20">
+        </div>
+        <div class="form-input">
+            <label for="confirmpassword">Congirm Password</label>
+            <input type="password" id="confirmpassword" name="confirmpassword" size="20">
+        </div>
+        <div class="form-input">
+            <label for="address">Address</label>
+            <input type="text" id="address" name="address" size="100">
+        </div>
+        <div class="form-input">
+            <label for="phonenum">Phone Number</label>
+            <input type="text" id="phonenum" name="phonenum" size="15">
+        </div>
+    </form>
+</body>
+</html>
