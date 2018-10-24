e8f63ed40eead4aeb6a35cce6814d6825b690fdc
Aufa45P <13516008@std.stei.itb.ac.id>
2018-10-25 02:12:46 +0700

    Search start

---
+++ view/search-books/index.php
@@ -0,0 +1,34 @@
+<?php
+    include ($_SERVER["DOCUMENT_ROOT"] . "/model/profile.php");
+    include ($_SERVER["DOCUMENT_ROOT"] . "/model/login.php");
+
+    if (!isset($_COOKIE["userId"])){
+        header("Location: /view/login?auth=false");
+        exit();
+    }
+    session_start();
+    $_SESSION["activeTab"] = "history";
+
+    //Get User From Database
+
+    $user = getUser($_COOKIE["userId"]);
+    $username = $user["username"];
+
+?>
+
+<html>
+    <head>
+        <title>Search Book</title>
+        <link rel="stylesheet" href="/assets/global/global.css">
+        <link rel="stylesheet" href="/assets/search/searchBook.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="container">
+            <?php include ($_SERVER[DOCUMENT_ROOT] . "/assets/header/header.php"); ?>
+            <h1>Search Book</h1>
+            <input type="text" id="search-txt" placeholder="input search terms"/>
+        </div>
+    </body>
+</html>
