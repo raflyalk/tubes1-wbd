856fbcdde1e17f14fb1fd4458aefb16aacc83378
rrifaldiu <13516098@std.stei.itb.ac.id>
2018-10-25 09:52:45 +0700

    add styling to review page

--- view/review/index.php
+++ view/review/index.php
@@ -43,8 +43,8 @@
     <head>
         <title>Review</title>
         <link rel="stylesheet" href="/assets/global/global.css">
-        <link rel="stylesheet" href="/view/history/history.css">
-        <!-- <link rel="stylesheet" href="/view/review/review.css"> -->
+        <!-- <link rel="stylesheet" href="/view/history/history.css"> -->
+        <link rel="stylesheet" href="/view/review/review.css">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     </head>
 
@@ -52,20 +52,23 @@
         <div class="container">
             <?php include ($_SERVER[DOCUMENT_ROOT] . "/assets/header/header.php"); ?>
             <div class="content">
-                <div class="left-book-detail">
-                    <div class="book-title"><h1>Nota Hidup</h1></div>
-                    <div class="left-book-details"> </div><h3>Light R. D. B.</h3>
+                <div class="flex-container">
+                    <div class="detail-holder">
+                        <div class="book-title"><h1>Nota Hidup</h1></div>
+                        <div class="book-detail"> </div><h3>Light R. D. B.</h3>
+                    </div>
+                <div class ="image-holder"><img src="https://store-images.s-microsoft.com/image/apps.34477.9007199266245907.4a4e2c37-33a9-40ae-8daf-c3b4776b9a89.41c21cd6-697f-4de5-b77c-4ef74a2bfedc?mode=scale&q=90&h=270&w=270&background=%234267B2" alt=""></div>
                 </div>
-                <div class ="right-book-image"><img src="https://store-images.s-microsoft.com/image/apps.34477.9007199266245907.4a4e2c37-33a9-40ae-8daf-c3b4776b9a89.41c21cd6-697f-4de5-b77c-4ef74a2bfedc?mode=scale&q=90&h=270&w=270&background=%234267B2" alt=""></div>
                 <h2>Add Rating</h2>
                 <div class="rating">Rating</div>
                 <h2>Add Comment</h2>
-                <textarea name="comment" id="comment" rows="10"></textarea>
-                <input type="button" id="back" value="Back">
-                <input type="button" id="submit" value="Submit">
-                <!-- <ul>
-                    
-                </ul> -->
+                <form action="">
+                    <textarea name="comment" id="comment" rows="5"></textarea>
+                    <span id="form-button">
+                        <input type="button" id="back" value="Back">
+                        <input type="button" id="submit" value="Submit">
+                    </div>
+                </form>
             </div>
         </div>
     </body>
