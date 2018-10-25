20748c626d4d1c0b78ad835c085a7c47be9d32bc
Aufa45P <13516008@std.stei.itb.ac.id>
2018-10-25 11:42:44 +0700

    Search Button Created & Profile Styling

--- view/search-books/index.php
+++ view/search-books/index.php
@@ -26,9 +26,10 @@
 
     <body>
         <div class="container">
-            <?php include ($_SERVER[DOCUMENT_ROOT] . "/assets/header/header.php"); ?>
-            <h1>Search Book</h1>
+            <?php include ($_SERVER['DOCUMENT_ROOT'] . "/assets/header/header.php"); ?>
+            <h1 class="head-txt">Search Book</h1>
             <input type="text" id="search-txt" placeholder="input search terms"/>
+            <button class="sub-button">Search</button>
         </div>
     </body>
 </html>
