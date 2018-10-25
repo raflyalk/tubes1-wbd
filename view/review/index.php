1a9a2ce2b00c312d593cb6b30465a845165b9e7f
rrifaldiu <13516098@std.stei.itb.ac.id>
2018-10-25 13:11:44 +0700

    add validation in review

--- view/review/index.php
+++ view/review/index.php
@@ -59,33 +59,35 @@
                     </div>
                 <div class ="image-holder"><img src="https://store-images.s-microsoft.com/image/apps.34477.9007199266245907.4a4e2c37-33a9-40ae-8daf-c3b4776b9a89.41c21cd6-697f-4de5-b77c-4ef74a2bfedc?mode=scale&q=90&h=270&w=270&background=%234267B2" alt=""></div>
                 </div>
-                <form action="">
-                <h2>Add Rating</h2>
-                <div class="rating">
-                    <!-- <span> -->
-                        <input type="radio" name="rating" id="star1" value="1"> 1
-                        <!-- <label for="star1"></label> -->
-                    <!-- </span>
-                    <span> -->
-                        <input type="radio" name="rating" id="star2" value="2"> 2
-                        <!-- <label for="star2"></label> -->
-                    <!-- </span>
-                    <span> -->
-                        <input type="radio" name="rating" id="star3" value="3"> 3
-                        <!-- <label for="star3"></label> -->
-                    <!-- </span>
-                    <span> -->
-                        <input type="radio" name="rating" id="star4" value="4"> 4
-                        <!-- <label for="star4"></label> -->
-                    <!-- </span>
-                    <span> -->
-                        <input type="radio" name="rating" id="star5" value="5"> 5
-                        <!-- <label for="star5"></label> -->
-                    <!-- </span> -->
-                <!-- <img src="/assets/images/blank-star-64.png" alt=""> -->
-                </div>
-                <h2>Add Comment</h2>
+                <form id="review-form" action="">
+                    <h2>Add Rating</h2>
+                    <div id="rating">
+                        <!-- <span> -->
+                            <input type="radio" name="rating" id="star1" value="1"> 1
+                            <!-- <label for="star1"></label> -->
+                        <!-- </span>
+                        <span> -->
+                            <input type="radio" name="rating" id="star2" value="2"> 2
+                            <!-- <label for="star2"></label> -->
+                        <!-- </span>
+                        <span> -->
+                            <input type="radio" name="rating" id="star3" value="3"> 3
+                            <!-- <label for="star3"></label> -->
+                        <!-- </span>
+                        <span> -->
+                            <input type="radio" name="rating" id="star4" value="4"> 4
+                            <!-- <label for="star4"></label> -->
+                        <!-- </span>
+                        <span> -->
+                            <input type="radio" name="rating" id="star5" value="5"> 5
+                            <!-- <label for="star5"></label> -->
+                        <!-- </span> -->
+                    <!-- <img src="/assets/images/blank-star-64.png" alt=""> -->
+                    </div>
+                    <div id="rating-validation"><!-- --></div>
+                    <h2>Add Comment</h2>
                     <textarea name="comment" id="comment" rows="5"></textarea>
+                    <div id="comment-validation"><!-- --></div>
                     <div id="form-button">
                         <input type="submit" name="action" id="back" value="Back">
                         <input type="submit" name="action" id="submit" value="Submit">
@@ -93,5 +95,6 @@
                 </form>
             </div>
         </div>
+        <script src="/view/review/script.js"></script>
     </body>
 </html>
--- view/review/script.js
+++ view/review/script.js
@@ -1,1 +1,48 @@
-var rating = document.getElementsByName("stars");
+function validateRating() {
+    var rating = document.getElementsByName('rating');
+    var checked = false;
+    for (var i = 0; i < rating.length; i++) {
+        if (rating[i].checked) {
+            checked = true;
+        }
+    }
+    var ratingValidation = document.getElementById('rating-validation');
+    if (! checked) {
+        console.log('here')
+        ratingValidation.innerText = 'This field is required';
+        return false;
+    } else {
+        console.log('here2')
+        ratingValidation.innerText = 't';
+        return true;
+    }
+}
+
+function validateComment() {
+    var comment_length = document.getElementById('comment').value.length;
+    var commentValidation = document.getElementById('comment-validation');
+    if (comment_length === 0) {
+        console.log('here3')
+        commentValidation.innerText = 'This field is required';
+        return false;
+    } else {
+        console.log('here4')
+        commentValidation.innerText = 't';
+        return true;
+    }
+}
+
+var rating = document.getElementsByName('rating');
+var comment = document.getElementById('comment');
+var reviewForm = document.getElementById('review-form');
+
+rating.onclick = validateRating();
+comment.onchange = validateComment();
+
+reviewForm.onsubmit = () => {
+    if (!(validateRating() || validateComment())) {
+        return false;
+    } else {
+        return true;
+    }
+}
