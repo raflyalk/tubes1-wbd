a2641faeb54ae765b55edf5c1bb3894dc7686b35
rrifaldiu <13516098@std.stei.itb.ac.id>
2018-10-25 11:48:13 +0700

    add rating button and initialize js in review

--- view/review/index.php
+++ view/review/index.php
@@ -59,14 +59,36 @@
                     </div>
                 <div class ="image-holder"><img src="https://store-images.s-microsoft.com/image/apps.34477.9007199266245907.4a4e2c37-33a9-40ae-8daf-c3b4776b9a89.41c21cd6-697f-4de5-b77c-4ef74a2bfedc?mode=scale&q=90&h=270&w=270&background=%234267B2" alt=""></div>
                 </div>
-                <h2>Add Rating</h2>
-                <div class="rating"><img src="/assets/images/blank-star-64.png" alt=""></div>
-                <h2>Add Comment</h2>
                 <form action="">
+                <h2>Add Rating</h2>
+                <div class="rating">
+                    <!-- <span> -->
+                        <input type="radio" name="rating" id="star1" value="1"> 1
+                        <!-- <label for="star1"></label> -->
+                    <!-- </span>
+                    <span> -->
+                        <input type="radio" name="rating" id="star2" value="2"> 2
+                        <!-- <label for="star2"></label> -->
+                    <!-- </span>
+                    <span> -->
+                        <input type="radio" name="rating" id="star3" value="3"> 3
+                        <!-- <label for="star3"></label> -->
+                    <!-- </span>
+                    <span> -->
+                        <input type="radio" name="rating" id="star4" value="4"> 4
+                        <!-- <label for="star4"></label> -->
+                    <!-- </span>
+                    <span> -->
+                        <input type="radio" name="rating" id="star5" value="5"> 5
+                        <!-- <label for="star5"></label> -->
+                    <!-- </span> -->
+                <!-- <img src="/assets/images/blank-star-64.png" alt=""> -->
+                </div>
+                <h2>Add Comment</h2>
                     <textarea name="comment" id="comment" rows="5"></textarea>
-                    <span id="form-button">
-                        <input type="button" id="back" value="Back">
-                        <input type="button" id="submit" value="Submit">
+                    <div id="form-button">
+                        <input type="submit" name="action" id="back" value="Back">
+                        <input type="submit" name="action" id="submit" value="Submit">
                     </div>
                 </form>
             </div>
---
+++ view/review/script.js
@@ -0,0 +1,1 @@
+var rating = document.getElementsByName("stars");
